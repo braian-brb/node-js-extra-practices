ae19bf7b1192be2306fb9159358e4eaf1c5fb5dc
Braian Bilicic <braian.bilicic@gmail.com>
2022-06-30 09:21:22 -0300

    update arrow function

---
+++ 02-leveling/destructuring/app.js
@@ -0,0 +1,15 @@
+// function add(num1, num2){
+//   return num1 + num2
+// }
+
+// They change little, mainly the difference lies in the THIS
+
+// const add = (num1, num2) => {
+//   return num1 + num2
+// }
+
+// == if it is resolved in a line the return is implicit
+
+const add = (num1, num2) => num1 + num2
+
+console.log(add(5, 15))
